d476409d27780e497fe9492e1079415ffbd216c7
Vincenzo Di Matteo <vincenzodimatteo@10-20-52-54.0200.fed.it.iosda.org>
2017-12-07 17:13:15 +0100

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Vincenzo Di Matteo on 07/12/2017.
+//  Copyright © 2017 Vincenzo Di Matteo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
